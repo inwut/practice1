ac8b0ab34eabfc2b9bb1a5a59c426a569c066671
Daria Vetrykush <dariavetrykush@gmail.com>
2024-05-19 14:00:47 +0300

    done testing SecureRandom

--- src/Main.java
+++ src/Main.java
@@ -1,28 +1,42 @@
 import java.nio.charset.StandardCharsets;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
-import java.util.Arrays;
+import java.security.SecureRandom;
 
 public class Main {
     public static void main(String[] args) {
         try {
             testMessageDigest();
+            testSecureRandom();
         } catch (NoSuchAlgorithmException e) {
             throw new RuntimeException();
         }
     }
 
+    public static void testSecureRandom() throws NoSuchAlgorithmException {
+        byte[] message = "Hello World".getBytes(StandardCharsets.UTF_8);
+        System.out.println(getSecureRandomHash(message, "SHA1PRNG"));
+        System.out.println(getSecureRandomHash(message, "DRBG"));
+        System.out.println(getSecureRandomHash(message, "Windows-PRNG"));
+    }
+
+    public static long getSecureRandomHash(byte[] message, String algorithm) throws NoSuchAlgorithmException {
+        SecureRandom secureRandom = SecureRandom.getInstance(algorithm);
+        secureRandom.nextBytes(message);
+        return secureRandom.nextLong();
+    }
+
     public static void testMessageDigest() throws NoSuchAlgorithmException {
         byte[] message = "Hello World".getBytes(StandardCharsets.UTF_8);
-        MessageDigest messageDigest = MessageDigest.getInstance("SHA-256");
+        System.out.println(getMessageDigestHash(message, "SHA-256"));
+        System.out.println(getMessageDigestHash(message, "SHA-384"));
+        System.out.println(getMessageDigestHash(message, "SHA-512"));
+    }
+
+    private static String getMessageDigestHash(byte[] message, String algorithm) throws NoSuchAlgorithmException {
+        MessageDigest messageDigest = MessageDigest.getInstance(algorithm);
         messageDigest.update(message);
-        System.out.println(convertToHex(messageDigest.digest()));
-        messageDigest = MessageDigest.getInstance("SHA-384");
-        messageDigest.update(message);
-        System.out.println(convertToHex(messageDigest.digest()));
-        messageDigest = MessageDigest.getInstance("SHA-512");
-        messageDigest.update(message);
-        System.out.println(convertToHex(messageDigest.digest()));
+        return convertToHex(messageDigest.digest());
     }
 
     private static String convertToHex(byte[] data) {
